a73c56ba56a459828c72e738a349a3a69fc14455
XiangyuMa <1056816866@qq.com>
2020-04-24 23:49:33 +0800

    两数之和两种解法

--- src/leetcode/editor/cn/[1]两数之和.java
+++ src/leetcode/editor/cn/[1]两数之和.java
@@ -15,6 +15,10 @@
 // Related Topics 数组 哈希表
 
 package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class TwoSum {
     public static void main(String[] args) {
         Solution solution = new TwoSum().new Solution();
@@ -23,10 +27,30 @@ class TwoSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
-
+    //暴力破解
     public int[] twoSum(int[] nums, int target) {
-        System.out.println("hello");
-        return nums;
+        for(int i = 0;i < nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    //hashmap查找
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i < nums.length;i++){
+                map.put(nums[i],i);
+        }
+        for(int i = 0;i<nums.length;i++){
+            int other = target - nums[i];
+            if(map.containsKey(other) && map.get(other) != i){
+                return new int[]{i,map.get(other)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
 }
