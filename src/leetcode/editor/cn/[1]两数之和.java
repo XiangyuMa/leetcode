79387712e023e1319d67cf4017dc24afff946268
XiangyuMa <1056816866@qq.com>
2020-05-02 21:19:01 +0800

    idea 下配置运行环境 commiter:maxiangyu

--- src/leetcode/editor/cn/[1]两数之和.java
+++ src/leetcode/editor/cn/[1]两数之和.java
@@ -1,17 +1,17 @@
 
-//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //
-// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 //
-// 
 //
-// 示例: 
+//
+// 示例:
 //
 // 给定 nums = [2, 7, 11, 15], target = 9
 //
 //因为 nums[0] + nums[1] = 2 + 7 = 9
 //所以返回 [0, 1]
-// 
+//
 // Related Topics 数组 哈希表
 
 package leetcode.editor.cn;
@@ -22,7 +22,12 @@ import java.util.Map;
 class TwoSum {
     public static void main(String[] args) {
         Solution solution = new TwoSum().new Solution();
-        solution.twoSum(null,0);
+        int nums[] = new int[]{2,34,54,232,54,6};
+        int arr[] =  solution.twoSum(nums,60);
+        for (int anArr : arr) {
+            System.out.println(anArr);
+        }
+
     }
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
