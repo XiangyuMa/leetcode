270287176ed3ded5c201d66aadcf61bdbb995993
XiangyuMa <1056816866@qq.com>
2020-05-04 14:38:58 +0800

    新增链表相加，链表值顺序与整数正好相反 commiter ：马翔宇

--- src/leetcode/editor/cn/RemoveElement.java
+++ src/leetcode/editor/cn/RemoveElement.java
@@ -18,6 +18,26 @@ public class RemoveElement {
         }
         return sentinode.next;
     }
+    //两个链表相加
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode node = new ListNode(0);
+        ListNode p1 = l1, p2 = l2, curNode = node;
+        int carry = 0;
+        while (p1 != null || p2 != null){
+            int x = p1 != null ? p1.val : 0;
+            int y = p2 != null ? p2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curNode.next = new ListNode(sum % 10);
+            curNode = curNode.next;
+            if (p1 != null)p1 = p1.next;
+            if (p2 != null)p2 = p2.next;
+        }
+        if (carry > 0)
+            curNode.next = new ListNode(carry);
+
+        return node.next;
+    }
     static class ListNode {
       int val;
       ListNode next;
